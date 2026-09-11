71dea8a02dd20e91d63ae4a4033163a0e9b44dc7
sha68236715hin <shahin236715@gmail.com>
2026-09-11 09:00:46 +0600

    footer responsive

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
    <Navbar/>
    <Banner/>
    <Products/>
-   {/* <Footer/> */}
+   <Footer/>
   </>
   )
 }
--- src/Component/Footer.tsx
+++ src/Component/Footer.tsx
@@ -5,14 +5,16 @@ const Footer = () => {
     <>
     
     <div className="mt-16 border-2 border-gray-300 p-4">
-      <div className="grid grid-cols-4 container mx-auto gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-4 container mx-auto gap-4">
         <div className="mt-4">
-          <img src={footerlogo} alt="logo" />
-          <p className="font-medium font-jakarta text-[12px] my-4">
+          <div className="flex justify-center md:block md:justify-start">
+          <img src={footerlogo} alt="logo"/>
+          </div>
+          <p className="font-medium font-jakarta text-[12px] my-4 text-center md:text-start">
             Curated tools, technologies, and resources for developers building
             modern software.
           </p>
-          <div className="flex gap-4">
+          <div className="flex justify-center md:justify-start gap-4">
             <h3 className="font-semibold font-jakarta text-[12px] my-4">GitHub</h3>
             <h3 className="font-semibold font-jakarta text-[12px] my-4">Twitter</h3>
             <h3 className="font-semibold font-jakarta text-[12px] my-4">LinkedIn</h3>
@@ -61,11 +63,11 @@ const Footer = () => {
     </div>
 
      <hr className="text-gray-300"/>
-      <div className="container mx-auto flex justify-between items-center py-2">
-        <h3>© 2026 Dev Stack. All rights reserved.</h3>
+      <div className="container mx-auto flex justify-between items-center py-2 px-2">
+        <h3 className="font-medium font-jakarta text-[12px]">© 2026 Dev Stack. All rights reserved.</h3>
         <div className="flex gap-4">
-          <h3>Privacy</h3>
-          <h3>Terms</h3>
+          <h3 className="font-medium font-jakarta text-[12px]">Privacy</h3>
+          <h3 className="font-medium font-jakarta text-[12px]">Terms</h3>
         </div>
       </div>
     </>
